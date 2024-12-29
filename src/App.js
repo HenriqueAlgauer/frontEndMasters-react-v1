7cfd47b0d7cf388f04f89c1622062833a52929f9
Pia <algauer.henrique@gmail.com>
2024-12-29 18:11:01 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const Pizza = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pepperoni Pizza"),
+    React.createElement("p", {}, "Mozzarella cheese, Pepperoni"),
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+  ])
+}
+
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement(App))
