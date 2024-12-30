baafede49e47dc7dc522dc70e49277419f08f4fa
Pia <algauer.henrique@gmail.com>
2024-12-29 21:11:20 -0300

    adicionado stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pepperoni, cheese" />
-      <Pizza name="Cheese" description="cheese" />
-      <Pizza name="Chicken" description="chicker, cheese" />
+      <Pizza name="Pepperoni" description="pepperoni, cheese" image={"/public/pizzas/pepperoni.webp"} />
+      <Pizza name="Cheese" description="cheese" image={"/public/pizzas/hawaiian.webp"} />
+      <Pizza name="Chicken" description="chicker, cheese" image={"/public/pizzas/big_meat.webp"} />
     </div>
   )
 }
